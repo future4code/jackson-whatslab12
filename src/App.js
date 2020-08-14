570bf8e8ef2ce70489f9428ec911cbc15893215f
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-14 14:19:15 -0300

    componetização de mensagens prontas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Mensagens from "./components/mensagens/MensagensComp"
+
+
+
+function App() {
+  return (
+    <div>
+        <Mensagens/>
+    </div>
+  );
+}
+
+export default App;
+  
---
+++ src/components/mensagens/MensagensComp.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export class Mensagens extends React.Component{
+    state = {
+        message: [
+            {
+            remetente: "Claudia",
+            mensagem: "Olar, tudo bom?"
+        },
+        {
+            remetente: "Yvini",
+            mensagem: "Oie. tudo sim. E vc?"
+        },
+        {
+            remetente: "Caio",
+            mensagem: "E ai, gente, tudo legal?"
+        }
+        ],
+        valorInputRemetente: "",
+        valorInputMensagem: ""
+    };
+
+    novoUsuario = () => {
+        const novaMensagem = {
+            remtente: this.state.valorInputRemetente,
+            mensagem: this.state.valorInputMensagem
+        };
+
+        const mensagemNova = [...this.state.message, novaMensagem];
+        this.setState({message: mensagemNova})
+    };
+
+    onChangeInputRemetente = (event) =>{
+        this.setState({valorInputRemetente: event.target.value});
+    };
+
+    onChangeInputMensagem = (event) =>{
+        this.setState({valorInputMensagem: event.target.value});
+    };
+
+    render() {
+        const listaDeMensagem = this.state.message.map ((messages) => {
+            return (
+                <p>
+                    {messages.remetente} - {messages.mensagem}
+                </p>
+            );
+        });
+
+        return (
+            <div>
+                {listaDeMensagem}
+            </div>
+          );
+    }
+};
+
+export default Mensagens;

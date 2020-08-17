ad66c32e5ff549bc05cfd10b4144852960557981
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-08-17 00:13:29 -0300

    vai

---
+++ build/precache-manifest.a8bb6a58937836efd7a7eed00a1bd3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f70dd751b21e1413306dda688282df2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25be1648574dbd532f5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b96733956614b57e882",
+    "url": "/static/js/2.448582c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.448582c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25be1648574dbd532f5",
+    "url": "/static/js/main.6c3e7142.chunk.js"
+  },
+  {
+    "revision": "2c41662f3543504d87f3",
+    "url": "/static/js/runtime-main.42276059.js"
+  },
+  {
+    "revision": "8a76f725bcb359928f80f1df4ebda248",
+    "url": "/static/media/whatslab.8a76f725.png"
+  }
+]);
